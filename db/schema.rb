0794487949d94753f290a7c4aab75c5aab704420
Matt Morris <mcm0027@yahoo.com>
2016-02-08 18:50:12 -0600

    create relationship between user and items

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160209000456) do
+ActiveRecord::Schema.define(version: 20160209004539) do
 
   create_table "items", force: true do |t|
     t.string   "name"
     t.boolean  "checked"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
